4623b9964c82fba2f3dd3080ee8c4bf4fb065de0
Ashik Liyakathali <ashik21395@gmail.com>
2020-01-19 15:47:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SimpleList from "./List/SimpleList";
+import ComplexList from "./List/ComplexList";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sample List</h1>
+      <SimpleList />
+      <hr />
+      <ComplexList />
     </div>
   );
 }
